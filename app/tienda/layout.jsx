19108b49534acbef5562857b07029936b45cd664
oscar-abud <oscarpalma.vpr2@gmail.com>
2026-08-16 20:54:19 -0400

    [FEAT] added roboto from next font google

--- app/tienda/layout.jsx
+++ app/tienda/layout.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import Link from "next/link";
+import { Roboto } from 'next/font/google';
 
 export const metadata = {
   title: "Mi Tienda oficial",
   description: "Tienda oficial de la reina",
 };
 
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  style: ["italic", "normal"],
+  subsets: ["latin"]
+})
+
 function TiendaLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={roboto.className}>
         Sub-Categorias
         <header>
           <Link href='/tienda/computadores'>Computadores</Link> <br />
